09c3c057d8428a9f211d5ebabc2c15092489dd50
Voxybuns <fossati.val@gmail.com>
2023-04-15 00:04:55 +0200

    better handle how lives are updated on screen

--- source/rooms/intermission.lua
+++ source/rooms/intermission.lua
@@ -4,6 +4,7 @@ local gfx <const> = pd.graphics
 
 class("Intermission").extends(Room)
 
+-- Create a table to store the lives on screen
 local dials = {}
 
 local sceneTimer
@@ -13,27 +14,37 @@ local sfxLost
 
 local isGameWon
 
+-- Create a class for the dial sprites
 class("Dial").extends(gfx.sprite)
 
+-- Initialize Dial object
 function Dial:init(__x, __y)
     self.dialImage = gfx.imagetable.new("assets/intermission/dial")
     self.dial = AnimatedSprite.new(self.dialImage)
-    self.dial:addState("idle", 1, 1)
     self.dial:addState("popped", 2, 2)
+    self.dial:addState("idle", 1, 1)
     self.dial:moveTo(__x, __y)
     self.dial:playAnimation()
 end
 
-function Dial:lose()
-    local sfx = pd.sound.sampleplayer.new("assets/popped_cork")
-    self.dial:changeState("popped")
-    sfx:play()
+-- Draw the life bar
+function checkLives()
+    -- Draw all dials with the default empty state
+    for i = 1, 4, 1 do
+        dials[i] = Dial(40 + (i * 24), 100)
+    end
+    -- Have all remaining lives display their idle state
+    for i = 1, gameSettings.lives, 1 do
+        dials[i].dial:changeState("idle")
+    end
 end
 
-function checkDials()
-    for i = gameSettings.lives, 0, -1 do
-        dials[i].dial:changeState("popped")
-    end
+-- Handle lives animation and count when game is lost
+function loseLive()
+    -- Changing the current life to its empty state
+    dials[gameSettings.lives].dial:changeState("popped")
+    -- Decreasing lives
+    gameSettings.lives -= 1
 end
 
 function Intermission:goToGame()
@@ -46,55 +57,45 @@ function Intermission:enter(previous, ...)
     --     TestGame2,
     --     TestGame3
     -- }
-
-    
-    for i = 1, 4, 1 do
-        dials[i] = Dial(40 + (i * 24), 100)
-    end
-
-    checkDials()
-
-    isGameWon = select(1, ...)
-    print(isGameWon)
-
+    -- Draw background image
     local backgroundImage = gfx.image.new("assets/intermission/background")
     gfx.sprite.setBackgroundDrawingCallback(
         function (x, y, width, height)
             backgroundImage:draw( 0, 0 )
         end
     )
-
+    -- Draw the lives counter
+    checkLives()
+    -- Store the exit game's state
+    isGameWon = select(1, ...)
+    -- Initialize SFX
     sfxIntermission = pd.sound.sampleplayer.new("assets/intermission")
     sfxWon = pd.sound.sampleplayer.new("assets/gamewon")
     sfxLost = pd.sound.sampleplayer.new("assets/gamelost")
-
-    if isGameWon == true then
+    -- React to exit game's state
+    if isGameWon == true then -- When exit game is won...
         sfxWon:play()
         sceneTimer = pd.timer.performAfterDelay(beatsToMs(8), function()
             Intermission:goToGame()
         end)
-    elseif isGameWon == false then
+    elseif isGameWon == false then -- When exit game is lost...
         sfxLost:play()
-        gameSettings.lives -= 1
-        checkDials()
+        loseLive()
         sceneTimer = pd.timer.performAfterDelay(beatsToMs(8), function()
             Intermission:goToGame()
         end)
     else
-        sfxIntermission:play()
+        sfxIntermission:play() -- When launching game for the first time...
         sceneTimer = pd.timer.performAfterDelay(beatsToMs(9.5), function()
             Intermission:goToGame()
         end)
     end
-
-
-
 end
 
+-- Debug functions to change tempo
 function Intermission:upButtonDown()
     gameSettings.tempo += 20
 end
-
 function Intermission:downButtonDown()
     gameSettings.tempo -= 20
 end
@@ -102,9 +103,4 @@ end
 function Intermission:AButtonDown()
     -- self.index = math.random(3)
     -- manager:enter(self.gameList[self.index](), gameSettings.tempo)
-    manager:enter(DiggingForGold())
-end
-
-function Intermission:BButtonDown()
-    dial4:lose()
 end
